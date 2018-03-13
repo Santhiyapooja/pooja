0ba88a3643a872c9b0f6937cb58640b674b9ef93
Santhiyapooja <37213726+Santhiyapooja@users.noreply.github.com>
2018-03-13 17:44:18 +0530

    Create vowelcon.py

---
+++ vowelcon.py
@@ -0,0 +1,7 @@
+str = input("Enter a character: ")
+
+if(str=='A' or str=='a' or str=='E' or str =='e' or str=='I'
+ or str=='i' or str=='O' or str=='o' or str=='U' or str=='u'):
+    print(str, "is a Vowel")
+else:
+    print(str, "is a Consonant")
