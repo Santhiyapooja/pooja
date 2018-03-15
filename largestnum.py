02b92ef7f7a87a48591a28bdb04430d79affb51f
Santhiyapooja <37213726+Santhiyapooja@users.noreply.github.com>
2018-03-15 16:55:43 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the values:"))
+b=int(input("Enter the values:"))
+c=int(input("Enter the values:"))
+if(a>=b and a>=c):
+  print(a)
+elif(b>=a and b>=c):
+  print(b)
+else:
+  print(c)
